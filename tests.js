15bba2d7b9ab003be961f739a0957365e907fc50
hdelavidaum <henrique.daum@baritecnologia.com.br>
2021-08-13 14:15:44 -0300

    chore: remove id from class and use it just as index to show after each request

--- tests.js
+++ tests.js
@@ -7,8 +7,7 @@ const bearerToken =
   "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiTUFTVEVSIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvZW1haWxhZGRyZXNzIjoiY29udGFiaWxpZGFkZUBiYXJpZ3VpcHJvbW90b3JhLmNvbS5iciIsIlVzZXJJZCI6IjEiLCJleHAiOjE2Mjg4OTk4NzcsImlzcyI6Ik5vdmkiLCJhdWQiOiJOb3ZpIn0.jHPFPCDS3OOGuY7RDXet1oCZWd8PwEWW2qUo79-IPmg";
 
 class RequestAnalytics {
-  constructor(beforeRequest, afterRequest, id) {
-    this.id = 0;
+  constructor(beforeRequest, afterRequest) {
     this.beforeRequest = beforeRequest;
     this.afterRequest = afterRequest;
     this.timeBetweenInSec = 0;
@@ -21,7 +20,7 @@ class RequestAnalytics {
 
 const fetchResults = [];
 
-const makeRequests = async (id) => {
+const makeRequests = async (index) => {
   const beforeRequest = Date.now();
 
   try {
@@ -32,7 +31,7 @@ const makeRequests = async (id) => {
       },
     })
       .then((response) => {
-        console.log(`request #${id} | status: ${response.status}`);
+        console.log(`request #${index} | status: ${response.status}`);
       })
       .catch((err) => {
         throw err;
@@ -42,7 +41,7 @@ const makeRequests = async (id) => {
   }
 
   const afterRequest = Date.now();
-  fetchResults.push(new RequestAnalytics(beforeRequest, afterRequest, id));
+  fetchResults.push(new RequestAnalytics(beforeRequest, afterRequest));
 };
 
 for (let i = 0; i < 50; i++) {
